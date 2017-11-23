c247fd55b1f1d26c66a554db2a98641254eb9207
Tomas Roy <hello@minicreative.net>
2017-11-23 18:21:58 -0500

    Spotify authetication success and failure working

---
+++ app/src/main/java/edu/unc/tomas/musicmap/AuthenticationSuccess.java
@@ -0,0 +1,13 @@
+package edu.unc.tomas.musicmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticationSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication_success);
+    }
+}
--- app/src/main/java/edu/unc/tomas/musicmap/MainActivity.java
+++ app/src/main/java/edu/unc/tomas/musicmap/MainActivity.java
@@ -1,16 +1,62 @@
 package edu.unc.tomas.musicmap;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 import values.MusicListFragment;
 import values.MusicMapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 8124;
+    private static final String CLIENT_ID = "f797993a6d4046a697e87452ac86afe8";
+    private static final String REDIRECT_URI = "musicmap://authenticationSuccess";
+    public String accessToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        chooseFragment("map");
+        updateStatus("Not connected to Spotify", false);
+        authenticateSpotify();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result is not cancelled & comes from the correct activity
+        if (resultCode != RESULT_CANCELED && requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            // Display error message on unsuccessful response
+            if (response.getError() != null) authenticationError();
+
+            // Save access token on successful response
+            else if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                updateStatus("Connected to Spotify!", false);
+                accessToken = response.getAccessToken();
+            }
+        }
+    }
+
+    // Bottom Navigation listener
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -28,15 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        chooseFragment("map");
-    }
-
+    // Choose Fragment: changes the view fragment
     protected void chooseFragment(String fragmentName) {
         android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         switch(fragmentName) {
@@ -50,6 +88,60 @@ public class MainActivity extends AppCompatActivity {
                 transaction.replace(R.id.rootLayout, new MusicMapFragment());
         }
         transaction.commit();
+    };
+
+    // Authentication Spotify: authenticates the current user
+    protected void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-playback-state", "user-read-currently-playing"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    // Update Status: updates status background and message
+    public void updateStatus(String message, Boolean error) {
+
+        // Get view elements
+        TextView statusText = (TextView) findViewById(R.id.statusText);
+        View status = (View) findViewById(R.id.status);
+
+        // Set message
+        if (statusText != null) statusText.setText(message);
+        else statusText.setText("");
+
+        // Set background color
+        if (error) status.setBackgroundResource(R.color.error);
+        else status.setBackgroundResource(R.color.gray);
+    };
+
+    // Authentication Error: handles an authentication error
+    public void authenticationError() {
+
+        updateStatus("Could not connect to Spotfy", true);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Failed to connect to Spotify");
+        alert.setMessage("Please ensure you have a working internet connection and then try again");
+        alert.setCancelable(true);
+        alert.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                authenticateSpotify();
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Log.v("DEBUG", "trying to cancel");
+                dialog.cancel();
+            }
+        });
+        alert.show();
+    }
+
+    // Status Click: handles click on status bar
+    public void statusClick(View view) {
+        if (accessToken == null) authenticateSpotify();
     };
 
 }
